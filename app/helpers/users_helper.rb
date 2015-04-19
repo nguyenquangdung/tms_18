dd5a0b4adb76e4c5505cc96d2156eee9b06c5698
Dung <dungnq164@gmail.com>
2015-04-19 07:26:21 +0700

    Sign up, login, logout, seeds for user and suppervisor

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+
+  # Returns the Gravatar for the given user.
+  def gravatar_for(user, options = { size: 80 })
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "http://static1.squarespace.com/static/543148b2e4b0134c795d2365/54f5b2ede4b04c82748c69d0/54f5b2ede4b04c82748c69d1/1425388270053/avatar-man.jpg"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
